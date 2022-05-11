7a9337b68e733af4786aad8ff0a99d422cf375ac
joaobenvindo <joaobenvindo93@gmail.com>
2022-05-11 01:01:14 +0000

    atualização v2

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-# print('Hello, world')
+from flask import Flask, render_template, request
+import cloudpickle
 
-from flask import Flask
+# with open('model.pkl', 'rb') as file_in:
+#  model_loaded = cloudpickle.load(file_in)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predicao', methods=["POST"])    
+def predicao():
+    nome = request.form['firstname']
+    predicao = model.predict(['firstname'])
+    return str(predicao[0])
+
+app.run(debug=True)
+
+
+# print(model.predict(['ruim']))
