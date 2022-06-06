c797510803a3bb126702ae3007bc7faf4fd1b47d
joaobenvindo <joaobenvindo93@gmail.com>
2022-06-06 23:12:19 +0000

    versão 3.0

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template, request
 import cloudpickle
+from flask import Flask, render_template, request
 
-# with open('model.pkl', 'rb') as file_in:
-#  model_loaded = cloudpickle.load(file_in)
+with open('nbacareer.pkl', 'rb') as file_in:
+  model = cloudpickle.load(file_in)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html', nome='Duração de Carreira - NBA')
 
 @app.route('/predicao', methods=['POST'])
 def predicao():
-  PD = request.form['Partidas Disputadas']
-  predicao = model.predict([PD])
+  gp = request.form['gp']
+  pts = request.form['pts']
+  fg = request.form['fg']
+  ft = request.form['ft']
+  reb = request.form['reb']
+  ast = request.form['ast']
+  stl = request.form['stl']
+  blk = request.form['blk']
+  tov = request.form['tov']
+
+  array=[[str(gp), str(pts), str(fg), str(ft), str(reb), str(ast), str(stl), str(blk), str(tov)]]
+
+  predicao = model.predict(array)
+
   return render_template('resposta.html', predicao=predicao[0])
 
 app.run(debug=True)
