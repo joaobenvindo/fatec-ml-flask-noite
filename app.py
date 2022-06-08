fbf791a2be0a41dc4e6f9a2774f229cd177355cb
joaobenvindo <joaobenvindo93@gmail.com>
2022-06-08 00:28:35 +0000

    versão 6.0

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ def index():
 
 @app.route('/predicao', methods=['POST'])
 def predicao():  
-  gp = request.form['gp']
-  pts = request.form['pts']
-  fg = request.form['fg']
-  ft = request.form['ft']
-  reb = request.form['reb']
-  ast = request.form['ast']
-  stl = request.form['stl']
-  blk = request.form['blk']
-  tov = request.form['tov']
+  gp = float(request.form['gp'])
+  pts = float(request.form['pts'])
+  fg = float(request.form['fg'])
+  ft = float(request.form['ft'])
+  reb = float(request.form['reb'])
+  ast = float(request.form['ast'])
+  stl = float(request.form['stl'])
+  blk = float(request.form['blk'])
+  tov = float(request.form['tov'])
 
-  predicao = model.predict([gp, pts, ft, reb, ast, stl, blk, tov])
+  predicao = model.predict([[gp, pts, fg, ft, reb, ast, stl, blk, tov]])
   return render_template('resposta.html', predicao=predicao[0])
 
 app.run(debug=True)
